b704b0563ea8b499e6e3f34b6a1516637030820b
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-21 14:45:41 +0200

    added routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Profile } from './pages/Profile';
+import { Contact } from './pages/Contact';
+import { Home } from './pages/Home';
+import { Error } from './pages/Error';
+import { Navbar } from './Navbar';
 
 function App() {
   return (
     <div className="App">
-
+     <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+     </Router>
     </div>
   );
 }
---
+++ src/pages/Error.js
@@ -0,0 +1,3 @@
+export const Error = () => {
+    return <h1 style={{color: 'red'}}>PAGE NOT FOUND</h1>
+}
