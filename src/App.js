809df8a30841c1fd51a80b50386d1971311b0596
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-21 16:18:46 +0200

    changed default option

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
 function App() {
-  const client = new QueryClient();
+  const client = new QueryClient({ defaultOptions: {
+      queries: {
+       refetchOnWindowFocus: false,
+      }
+    }
+  });
 
   return (
     <div className="App">
