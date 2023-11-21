afb9936d69b17a575be76a33adef7ba6cecf2afb
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-21 16:09:26 +0200

    added more interesting stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,22 @@ import { useQuery } from "@tanstack/react-query";
 import Axios from 'axios';
 
 export const Home = () => {
- const {data} = useQuery(['cat'], () => {
+ const {data: catData, isLoading, isError, refetch} = useQuery(['cat'], () => {
   return Axios.get('https://catfact.ninja/fact').then((res) => res.data);
  });
 
+ if(isLoading) {
+    return <h1>Loading...</h1>
+ }
+
+ if(isError) {
+    return <h1>Sorry there was an error</h1>
+ }
+
     return (
      <h1>
-        This is the home page: <p>{data?.fact}</p>
+        This is the home page: <p>{catData?.fact}</p>
+        <button onClick={refetch}>Update Fact</button>
      </h1>
     );
 };
